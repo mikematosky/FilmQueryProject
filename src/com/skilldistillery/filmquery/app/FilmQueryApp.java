747c822631eb156911cf1884eab677c505323b57
mikematosky <mikematosky@hotmail.com>
2020-05-03 00:29:06 -0400

    enough setup for tonight

--- src/com/skilldistillery/filmquery/app/FilmQueryApp.java
+++ src/com/skilldistillery/filmquery/app/FilmQueryApp.java
@@ -12,25 +12,65 @@ public class FilmQueryApp {
 
   public static void main(String[] args) {
     FilmQueryApp app = new FilmQueryApp();
-    app.test();
-//    app.launch();
-  }
-
-  private void test() {
-    Film film = db.findFilmById(1);
-    System.out.println(film);
+    app.launch();
   }
 
   private void launch() {
-    Scanner input = new Scanner(System.in);
     
-    startUserInterface(input);
+	  Scanner input = new Scanner(System.in);
     
-    input.close();
+	  	welcome();
+	  	startUserInterface(input);
+	  
+	  input.close();
   }
 
   private void startUserInterface(Scanner input) {
+	  boolean dontExit= true;
+	  String selection= "";
+	  
+	  
+	  
+	  
+	  MENU: while(dontExit) {
+		  //semi GUI
+		  System.out.println("|| -- Please make a selection from the menu -- ||");
+		  System.out.println("||                                             ||");
+		  System.out.println("|| '1': Search film by ID                      ||");
+		  System.out.println("|| '2': Search film by Keyword                 ||");
+		  System.out.println("|| '3': Exit                                   ||");
+		  System.out.println("||                                             ||");
+		  System.out.println("|| Your selection: ");
+		  
+		  selection= input.next();
+		  
+		  switch(selection) {
+		  case "1":
+			  break;
+		  case "2":
+			  break;
+		  case "3":
+			  dontExit= false;
+			  break;
+		  default:
+			  System.out.println("###@ Not a valid input @###");
+			  continue MENU;
+		  		
+		  }
+		  
+		  
+	  }
     
+  }
+  
+  private void welcome() {
+	  System.out.println("||");
+	  System.out.println("||");
+	  System.out.println("||");
+	  System.out.println("||  Welcome to Film Query Project");
+	  System.out.println("||");
+	  System.out.println("||");
+	  System.out.println("||");
   }
 
 }
